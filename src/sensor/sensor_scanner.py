8e9b99b012419f1e34fd367368e8c0adadec2352
benedekaibas <kaibas.benedek02@gmail.com>
2025-08-09 12:27:40 +0200

    edit: file reconstructed

--- src/sensor/sensor_scanner.py
+++ src/sensor/sensor_scanner.py
@@ -1,13 +1,29 @@
 import serial
 import time
 
-srl = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-srl.write(b"R\r")
+def connect_sensor():
+    srl = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    return srl
 
-time.sleep(1)
+def single_read():
+    """Get a single reading from the sensor to see if connection was successful."""
+    srl = connect_sensor()
+    srl.write(b"R\r")
 
-while(srl.in_waiting):
-    print(srl.readline().decode().split())
+    time.sleep(1) # time sleep for not overwhelming the sensor, but it might need to remove
+    while(srl.in_waiting):
+        print(srl.readline().decode().split())
 
-srl.close()
+    srl.close()
 
+def continuous_reading():
+    """Receive continuous reading from the sensor."""
+    srl = connect_sensor()
+    srl.write(b"C,5\r")
+
+    return srl
+
+
+if __name__ == "__main__":
+    res = continuous_reading()
+    print(res)
