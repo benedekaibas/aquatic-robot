15bf27babf9be0b9d58e3e1c2abc118d965881a6
benedekaibas <kaibas.benedek02@gmail.com>
2025-08-13 19:12:02 +0200

    build: storing & calling ports function changed

--- src/sensor/sensor_scanner.py
+++ src/sensor/sensor_scanner.py
@@ -3,32 +3,43 @@ import time
 
 from serial import SerialException, SerialTimeoutException
 
-def connect_sensor():
-        srl = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-        return srl
+ports = {
+            "ph": 'dev/ttyUSB0',
+            "do": 'dev/ttyUSB1',
+            "orp": 'dev/ttyUSB2',
+            "ec": 'dev/ttyUSB3',
+            "rtd": 'dev/ttyUSB4'
+
+            }
+
+def store_ports():
+    ports_list = []
+
+    ph = serial.Serial('dev/ttyUSB0', 9600, timeout=1)
+    ports_list.append(ph)
+    do = serial.Serial('dev/ttyUSB1', 9600, timeout=1)
+    ports_list.append(do)
+    orp = serial.Serial('dev/ttyUSB2', 9600, timeout=1)
+    ports_list.append(orp)
+    ec = serial.Serial('dev/ttyUSB3', 9600, timeout=1)
+    ports_list.append(ec)
+    rtd = serial.Serial('dev/ttyUSB4', 9600, timeout=1)
+    ports_list.append(rtd)
+
+    return ports_list
 
 def single_read():
     """Get a single reading from the sensor to see if connection was successful."""
-    srl = connect_sensor()
-    srl.write(b"R\r")
+    ports = store_ports()
+    for port in ports:
+        port.write(b"R\r")
 
     time.sleep(1) # time sleep for not overwhelming the sensor, but it might need to remove
-    while(srl.in_waiting):
-        print(srl.readline().decode().split())
+    while(port.in_waiting):
+        print(port.readline().decode().split())
 
-    srl.close()
-
-def continuous_reading():
-    """Receive continuous reading from the sensor."""
-    srl = connect_sensor()
-    print("pH readings: ")
-    srl.write(b"C,5\r")
-    return srl
+    port.close()
 
 
 if __name__ == "__main__":
-    try:
-        connect_sensor()
-        continuous_reading()
-    except SerialException:
-        print("Could not connect to the sensor...")
+    single_read()
