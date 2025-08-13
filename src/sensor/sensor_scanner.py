3b0b6d8b80ad8ed1c73a34667a93e88c2892ddd3
benedekaibas <kaibas.benedek02@gmail.com>
2025-08-13 19:26:04 +0200

    fix: port calls correctd & unused dict removed

--- src/sensor/sensor_scanner.py
+++ src/sensor/sensor_scanner.py
@@ -3,27 +3,20 @@ import time
 
 from serial import SerialException, SerialTimeoutException
 
-ports = {
-            "ph": 'dev/ttyUSB0',
-            "do": 'dev/ttyUSB1',
-            "orp": 'dev/ttyUSB2',
-            "ec": 'dev/ttyUSB3',
-            "rtd": 'dev/ttyUSB4'
 
-            }
 
 def store_ports():
     ports_list = []
 
-    ph = serial.Serial('dev/ttyUSB0', 9600, timeout=1)
+    ph = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ports_list.append(ph)
-    do = serial.Serial('dev/ttyUSB1', 9600, timeout=1)
+    do = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
     ports_list.append(do)
-    orp = serial.Serial('dev/ttyUSB2', 9600, timeout=1)
+    orp = serial.Serial('/dev/ttyUSB2', 9600, timeout=1)
     ports_list.append(orp)
-    ec = serial.Serial('dev/ttyUSB3', 9600, timeout=1)
+    ec = serial.Serial('/dev/ttyUSB3', 9600, timeout=1)
     ports_list.append(ec)
-    rtd = serial.Serial('dev/ttyUSB4', 9600, timeout=1)
+    rtd = serial.Serial('/dev/ttyUSB4', 9600, timeout=1)
     ports_list.append(rtd)
 
     return ports_list
