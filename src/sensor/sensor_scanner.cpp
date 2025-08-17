901a19d7ab23d1509a9991c6650bc3f98838fdfc
kaibas01 <kaibas01@allegheny.edu>
2025-08-17 17:09:48 -0400

    refactor: changed method of accessing members of the struct

--- src/sensor/sensor_scanner.cpp
+++ src/sensor/sensor_scanner.cpp
@@ -3,13 +3,28 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <termios.h>
-#include <unistd.h> 
+//#include <unistd.h> 
 
 struct Ports {
     const char* sensor;
     const char* port;
 };
 
+auto get_ports() {
+    Ports ports;
+
+    ports.sensor = "ph";
+    ports.port = "/dev/ttyUSB0";
+
+    std::vector<const char*> vec_port = {ports.sensor, ports.port};
+
+    for (const char* p: vec_port) {
+        int serial_port;
+        const char* serial_port_ptr = ports.sensor;
+        serial_port = open(serial_port_ptr, O_RDWR);
+        std::cout << serial_port;
+    }
+}
 
 auto read_ports() {
     std::vector<Ports> ports = {
