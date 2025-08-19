516ffa9b5fce7a7ea91be695087a920d69d778f5
benedekaibas <kaibas.benedek02@gmail.com>
2025-08-19 15:22:36 +0200

    fix: loop error fixed, so now we get real values

--- src/sensor/do_sensor.py
+++ src/sensor/do_sensor.py
@@ -6,28 +6,19 @@ import serial
 
 
 
-def do_port():
-    """Storing dissolved oxygen sensor's value in a list."""
-    do_port = []
-
-    do_value = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
-    do_port.append(do_value)
-
-    return do_port
-
 def read_do_port():
-    """Get readings from the do sensor."""
-    do_value = do_port()
+    """Read dissolved oxygen value."""
+    do_value = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
 
     while True:
-        for do in do_value:
-            do.write(b"R\r")
-            time.sleep(0.03)
+        do_value.write(b"R\r")
+        time.sleep(0.03)
 
-            if do.in_waiting > 0:
-                data = do.readline().decode().split()
-                print(f"Do {do} level: ", {data})
-            time.sleep(0.01)
+        if do_value.in_waiting > 0:
+            data = do_value.readline().decode().split()
+            print(f"{do_value}: {data}")
+
+        time.sleep(0.01)
 
 if __name__ == "__main__":
     read_do_port()
