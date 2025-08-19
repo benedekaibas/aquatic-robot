5d6ab1690498319555411b4c2660d944424123ff
benedekaibas <kaibas.benedek02@gmail.com>
2025-08-19 15:49:23 +0200

    fix: logic of accessing to serial port updated

--- src/sensor/do_sensor.py
+++ src/sensor/do_sensor.py
@@ -10,15 +10,16 @@ def read_do_port():
     """Read dissolved oxygen value."""
     do_value = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
 
-    while True:
+    do_value.write(b"R\r")
+    time.sleep(0.03)
+    count = 0
+
+    while(count < 10):
         do_value.write(b"R\r")
-        time.sleep(0.03)
+        count += 1
+        data = do_value.readline().decode().split()
+        print(f"{do_value}: {data}")
 
-        if do_value.in_waiting > 0:
-            data = do_value.readline().decode().split()
-            print(f"{do_value}: {data}")
-
-        time.sleep(0.01)
 
 if __name__ == "__main__":
     read_do_port()
