410dd4dede1faced17255f8ce59ac0bb607f0b35
benedekaibas <kaibas.benedek02@gmail.com>
2025-08-20 13:20:27 +0200

    bug: try to read content of the buffer not just the memory value

--- src/sensor/do_sensor.cpp
+++ src/sensor/do_sensor.cpp
@@ -8,8 +8,9 @@
 #include <termios.h> // Contains POSIX terminal control definitions
 #include <unistd.h> // write(), read(), close()
 
-int open_serial_port() {
-  int serial_port = open("/dev/ttyUSB1", O_RDWR);
+void open_serial_port() {
+  std::string port = "/dev/ttyUSB1";
+  int serial_port = open(port.c_str(), O_RDWR);
 
   if(serial_port < 0) {
     std::cerr << "Error while opning the port: " << strerror(errno);
@@ -18,9 +19,23 @@ int open_serial_port() {
   }
 
   char read_buffer[256];
-  int num_bytes = read(serial_port, &read_buffer, sizeof(read_buffer));
+  std::string response;
 
-  return num_bytes;
+
+  do {
+    int num_bytes = read(serial_port, &read_buffer, sizeof(read_buffer));
+
+    if(num_bytes > 0) {
+      response += std::string(read_buffer);
+      std::cout << read_buffer;
+    }
+
+  } while(read_buffer[0] != 'X');
+
+  std::cout << "Response: " << "\n";
+  std::cout << response;
+
+  close(serial_port);
 }
 
 int main() {
